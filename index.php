a43987bdd1cfa804e9f23a9d48cbc974b335b939
Giorgio Andrei <cipuandrei@libero.it>
2021-09-21 17:09:08 +0200

    word-can-now-be-censored

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
+$word = strtoupper($_GET["word"]); 
 $sator_square = 'SATOR AREPO TENET OPERA ROTAS';
+
+$sator_square = str_replace($word, '***', $sator_square);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +13,8 @@ $sator_square = 'SATOR AREPO TENET OPERA ROTAS';
     <title>BadWords</title>
 </head>
 <body>
+
+
     <h1><?php echo $sator_square ?></h1>
 </body>
 </html>
